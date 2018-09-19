1c70c9e1470affbdeaecf23e8eff713275c29e0e
MicahWeiss <micahjweiss@gmail.com>
2018-09-19 16:08:01 -0500

    functional stack complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,21 +1,27 @@
 var Stack = function() {
   var someInstance = {}; //big object that will be returned
-
-  // Use an object with numeric keys to store values
-  var storage = {}; //mjw- is this gonna be someInstance[storage] = {1: a, 2: blargle, 3: blargles}?
+  var storage = {}; 
   var size = 0;
 
   // Implement the methods below
+
   someInstance.push = function(value) {
-    //adds item to stac (highest number in storage obj)
-    //size++
+    storage[size] = value;//adds item to stack (highest number in storage obj)
+    size++;
   };
 
   someInstance.pop = function() {
-    //removes item -- (makes highest number key in storage have undefined value)
+    let popped;
+    if(size > 0){ 
+      popped = storage[size-1];
+      delete storage[size-1];
+      size--;
+    }
+    return popped;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
