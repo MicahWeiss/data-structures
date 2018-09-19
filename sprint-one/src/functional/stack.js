6e0db8e9228b8716841256912477e7e0bcb50770
MicahWeiss <micahjweiss@gmail.com>
2018-09-19 15:24:39 -0500

    Testing

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,14 +1,18 @@
 var Stack = function() {
-  var someInstance = {};
+  var someInstance = {}; //big object that will be returned
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {}; //mjw- is this gonna be someInstance[storage] = {1: a, 2: blargle, 3: blargles}?
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //adds item to stac (highest number in storage obj)
+    //size++
   };
 
   someInstance.pop = function() {
+    //removes item -- (makes highest number key in storage have undefined value)
   };
 
   someInstance.size = function() {
