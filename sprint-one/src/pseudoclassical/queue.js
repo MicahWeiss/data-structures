a58cf5e8455a2f75759c726511363d3cf95ca706
MicahWeiss <micahjweiss@gmail.com>
2018-09-20 10:24:58 -0500

    add queue pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this._size = 0;
 };
 
 
+ Queue.prototype.enqueue = function(value){
+    for(var i=this._size-1; i>=0; i--) {
+      this.storage[i+1] = this.storage[i];
+    }
+    this.storage[0] = value;
+    this._size++;
+  };
+  
+  Queue.prototype.dequeue = function(){
+    if (this._size === 0) {
+      return undefined;
+    }
+
+    var tmp = this.storage[this._size-1];
+    delete this.storage[this._size-1];
+    this._size--;
+    return tmp;
+  };
+
+  Queue.prototype.size =  function(){
+    return this._size;
+  }
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this._size = 0;
 };
 
 
+Stack.prototype.push = function(value){
+    this.storage[this._size] = value;//adds item to stack (highest number in storage obj)
+    this._size++; 
+  };
+Stack.prototype.pop = function(){
+    let popped;
+    if(this._size > 0){ 
+      popped = this.storage[this._size - 1];
+      delete this.storage[this._size - 1];
+      this._size--;
+    }
+    return popped;
+  };
+
+Stack.prototype.size = function(){
+    return this._size;
+  };
