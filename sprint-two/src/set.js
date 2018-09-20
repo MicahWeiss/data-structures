9206244c48ce2b51a056a885b3371ea7621d57bb
MicahWeiss <micahjweiss@gmail.com>
2018-09-20 17:17:27 -0500

    set complete

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; 
   return set;
 };
 
-var setPrototype = {};
-
-setPrototype.add = function(item) {
+var setPrototype = {
+  add : function(item) {
+    this._storage[item]=item;
+  },
+  
+  contains : function(item) {
+    return Boolean(this._storage[item]);
+  },
+  
+  remove : function(item) {
+    delete this._storage[item];
+  }
 };
 
-setPrototype.contains = function(item) {
-};
 
-setPrototype.remove = function(item) {
-};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * everything is O(1)
  */
+
+
