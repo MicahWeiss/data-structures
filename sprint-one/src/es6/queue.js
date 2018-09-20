7592a06608de29261da90f66624683cc9c10db2e
MicahWeiss <micahjweiss@gmail.com>
2018-09-20 10:34:49 -0500

    add Queue & stack ES6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,7 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-    //.whatever properties
+    this.storage = {};
+    this._size = 0;
   }
   //methods
+  enqueue(value){
+    for(var i=this._size-1; i>=0; i--) {
+      this.storage[i+1] = this.storage[i];
+    }
+    this.storage[0] = value;
+    this._size++;
+  };
+  
+  dequeue(){
+    if (this._size === 0) {
+      return undefined;
+    }
+
+    var tmp = this.storage[this._size-1];
+    delete this.storage[this._size-1];
+    this._size--;
+    return tmp;
+  };
+
+  size(){
+    return this._size;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,9 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-    //
+    this.storage = {};
+    this._size = 0;
   }
-  //function push(string)
-  //function pop()
-  //function size()
+  push(value) {
+    this.storage[this._size] = value;//adds item to stack (highest number in storage obj)
+    this._size++; 
+  }
+  pop() {
+    let popped;
+    if(this._size > 0){ 
+      popped = this.storage[this._size - 1];
+      delete this.storage[this._size - 1];
+      this._size--;
+    }
+    return popped;
+  }
+  size() {
+    return this._size;
+  }
 }
