a3e6118baf29ae79f6800ab33b1eb2ab8294290a
MicahWeiss <micahjweiss@gmail.com>
2018-09-20 15:57:16 -0500

    Add graph comments

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -56,6 +56,13 @@ Graph.prototype.forEachNode = function(cb) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addnode O(1)
+ * contains O(1)
+ * removeNode O(n)
+ * hasEdge O(1)
+ * addEdge O(1)
+ * removeEdge O(1)
+ * forEachNode O(N)
  */
 
 
