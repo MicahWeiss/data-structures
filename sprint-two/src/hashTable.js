8b2bb7707aadfd88808efd9fda933c894ce5f5c0
Chris Athanas <realityexpander@gmail.com>
2018-09-21 10:58:51 -0500

    Hashtable complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,20 +5,43 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  //collision handling
+  while (
+    this._storage.get(index) !== undefined &&
+    this._storage.get(index)[0] !== k
+  ) {
+    index++;
+    index = index % this._limit;
+  }
+  //output
+  this._storage.set(index, [k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //collision handling
+  while (
+    this._storage.get(index) !== undefined &&
+    this._storage.get(index)[0] !== k
+  ) {
+    index++;
+    index = index % this._limit;
+  }
+
+  //output
+  if (this._storage.get(index) !== undefined) {
+    return this._storage.get(index)[1];
+  } else {
+    return undefined;
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
