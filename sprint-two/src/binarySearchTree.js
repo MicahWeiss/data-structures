110f85792fcce07161b85e67b5e101ee9d005967
Chris Athanas <realityexpander@gmail.com>
2018-09-21 16:17:46 -0500

    implement BST spec

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,90 @@
 var BinarySearchTree = function(value) {
+  var _tree = { value: value, left: undefined, right: undefined };
+  Object.assign(_tree, treePrototype);
+  return _tree;
 };
 
+var treePrototype = {
+  insert: function(value) {
+    var currentNode = this;
+
+    while (value !== currentNode.value) {
+      if (value < currentNode.value) {
+        if (currentNode.left !== undefined) {
+          currentNode = currentNode.left;
+        } else {
+          currentNode.left = {
+            value: value,
+            left: undefined,
+            right: undefined
+          };
+          return;
+        }
+      } else if (value > currentNode.value) {
+        if (currentNode.right !== undefined) {
+          currentNode = currentNode.right;
+        } else {
+          currentNode.right = {
+            value: value,
+            left: undefined,
+            right: undefined
+          };
+          return;
+        }
+      }
+    }
+    //DO NOTHING
+  },
+
+  contains: function(value) {
+    var currentNode = this;
+    while (value !== currentNode.value) {
+      if (value < currentNode.value) {
+        if (currentNode.left !== undefined) {
+          currentNode = currentNode.left;
+        } else {
+          return false;
+        }
+      } else if (value > currentNode.value) {
+        if (currentNode.right !== undefined) {
+          currentNode = currentNode.right;
+        } else {
+          return false;
+        }
+      }
+    }
+    return true;
+  },
+
+  depthFirstLog: function(func) {
+    var currentNode = this;
+
+    function recursiveDFL(currentNode) {
+      func(currentNode.value);
+      if (currentNode.left !== undefined) {
+        recursiveDFL(currentNode.left);
+      }
+      if (currentNode.right !== undefined) {
+        recursiveDFL(currentNode.right);
+      }
+    }
+
+    recursiveDFL(currentNode);
+  }
+};
+
+//find: function(value) {
+/*return the  that contains the item
+    let currentNode = root
+    if(value < currentNode.val){
+         if(currentNode.left !== undefined){    
+             currentNode = currentNode.left
+         }else{
+             return 
+         }
+    }
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
